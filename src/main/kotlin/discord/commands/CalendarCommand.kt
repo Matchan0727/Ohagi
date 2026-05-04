3c8b7bdecdf1829f4ab421f3bd38c0c4f249b2e8
Matchan0727 <matchan0727ahaha@gmail.com>
2026-05-04 23:58:53 +0900

    update: コンテキストを変更し、全てのDMでコマンドが利用できるようになり、ユーザーインストールコマンドとしても使えるようになった

--- src/main/kotlin/discord/commands/CalendarCommand.kt
+++ src/main/kotlin/discord/commands/CalendarCommand.kt
@@ -3,11 +3,13 @@ package command
 import calendar.CalendarInfoGenerator
 import com.jagrosh.jdautilities.command.SlashCommand
 import com.jagrosh.jdautilities.command.SlashCommandEvent
+import net.dv8tion.jda.api.interactions.InteractionContextType
 
 class CalendarCommand : SlashCommand() {
     init {
         this.name = "calendar"
         this.help = "いろんな人の予定を表示します"
+        this.contexts = InteractionContextType.ALL.toTypedArray()
     }
 
     override fun execute(event: SlashCommandEvent) {
