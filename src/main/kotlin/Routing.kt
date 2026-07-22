ed2729157d635b5cfc6333f9352b152b151fd31d
Matchan0727 <matchan0727ahaha@gmail.com>
2026-07-23 01:44:50 +0900

    update: webhook受け取り確認

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,13 +1,26 @@
 package jp.simplespace
 
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.request.receive
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.websocket.WebSocketDeflateExtension.Companion.install
+import kotlinx.serialization.json.Json
+import java.util.logging.Logger
 
 fun Application.configureRouting() {
+    install(ContentNegotiation) {
+        json()
+    }
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+        post("/switchbot") {
+            val request = call.receive<Json>()
+            Logger.getGlobal().info("Switchbot request received: $request")
         }
     }
 }
