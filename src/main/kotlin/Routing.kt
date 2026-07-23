111bed964840ba33ab7ac2250d2c0ceddbf5703e
Matchan0727 <matchan0727ahaha@gmail.com>
2026-07-23 11:13:26 +0900

    fix

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -2,6 +2,7 @@ package jp.simplespace
 
 import io.ktor.server.application.*
 import io.ktor.server.request.receive
+import io.ktor.server.request.receiveText
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
@@ -13,7 +14,7 @@ fun Application.configureRouting() {
             call.respondText("Hello World!")
         }
         post("/switchbot") {
-            val request = call.receive<Json>()
+            val request = call.receiveText()
             Logger.getGlobal().info("Switchbot request received: $request")
         }
     }
